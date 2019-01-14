34b33a7a8e3e40f14c3cfcdaa78d40f770707ee8
MistakeNot4892 <AGMogett@gmail.com>
2019-01-14 16:03:45 +1100

    Added list and additional_tiles checking.

--- PawnUtil.py
+++ PawnUtil.py
@@ -8,74 +8,84 @@ from collections import OrderedDict
 
 ## Init values.
 usageString =    'Usage: ' + os.path.basename(__file__) + ' [input file] [starting value] [offset value] [output file]'
-startingValue =  0
-offsetValue =    0
-inputFilename =  'input.json'
-outputFilename = 'output.json'
+offsets = { 'fg': 0, 'bg': 0 }
 
-## Get input filename.
-if len(sys.argv) < 2:
-	print(usageString)
-	sys.exit()
-inputFilename = sys.argv[1]
-if not os.path.exists(inputFilename):
-	print('Please specify an input file that exists.')
-	sys.exit()
+def updateFgAndBgValuesInList(input_list, offsetValue, startingValue):
+	for ind in range(len(input_list)):
+		for keyStr in offsets:
+			if keyStr in input_list[ind]:
+				try:
+					if type(input_list[ind][keyStr]) is list:
+						for subind in range(len(input_list[ind][keyStr])):
+							if int(input_list[ind][keyStr][subind]) > startingValue:
+								input_list[ind][keyStr][subind] += offsetValue
+								offsets[keyStr] += 1
+					else:
+						if int(input_list[ind][keyStr]) > startingValue:
+							input_list[ind][keyStr] += offsetValue
+							offsets[keyStr] += 1
+				except ValueError:
+					print('Invalid value in JSON parse (ind ' + str(ind) + ', key ' + keyStr + ') - not int or list.')
+		if 'additional_tiles' in input_list[ind]:
+			updateFgAndBgValuesInList(input_list[ind]['additional_tiles'], offsetValue, startingValue)
 
-## Get starting value.
-if len(sys.argv) < 3:
-	print(usageString)
-	sys.exit()
-try:
-	startingValue = int(sys.argv[2])
-except ValueError:
-	print('Please specify an integer value for the starting value.')
-	sys.exit()
+def main():
 
-## Get offset value.
-if len(sys.argv) < 4:
-	print(usageString)
-	sys.exit()
-try:
-	offsetValue = int(sys.argv[3])
-except ValueError:
-	print('Please specify an integer value for the offset value.')
-	sys.exit()
+	startingValue =  0
+	offsetValue =    0
+	inputFilename =  'input.json'
+	outputFilename = 'output.json'
 
-## Get output filename.
-if len(sys.argv) >= 4:
-	outputFilename = sys.argv[4]
+	## Get input filename.
+	if len(sys.argv) < 2:
+		print(usageString)
+		sys.exit()
+	inputFilename = sys.argv[1]
+	if not os.path.exists(inputFilename):
+		print('Please specify an input file that exists.')
+		sys.exit()
 
-## Done with arg validation, process the json.
-print('Offsetting all fg/bg values higher than ' + str(startingValue) + ' by ' + str(offsetValue) + ' in file ' + inputFilename + '.')
-inputFile = open(inputFilename, 'r')
-loadedJson = json.loads(inputFile.read(), object_pairs_hook = OrderedDict)
-inputFile.close()
+	## Get starting value.
+	if len(sys.argv) < 3:
+		print(usageString)
+		sys.exit()
+	try:
+		startingValue = int(sys.argv[2])
+	except ValueError:
+		print('Please specify an integer value for the starting value.')
+		sys.exit()
 
-offsetFg = 0
-offsetBg = 0
-for i in range(len(loadedJson['tiles-new'][0]['tiles'])):
-	checkingJson = loadedJson['tiles-new'][0]['tiles'][i]
-	if 'fg' in checkingJson and int(checkingJson['fg']) > startingValue:
-		loadedJson['tiles-new'][0]['tiles'][i]['fg'] += offsetValue
-		offsetFg += 1
-	if 'bg' in checkingJson and int(checkingJson['bg']) > startingValue:
-		loadedJson['tiles-new'][0]['tiles'][i]['bg'] += offsetValue
-		offsetBg += 1
-	if 'additional_tiles' in checkingJson:
-		for j in range(len(checkingJson['additional_tiles'])):
-			additionalJson = checkingJson['additional_tiles'][j]
-			if 'fg' in additionalJson and int(additionalJson['fg']) > startingValue:
-				additionalJson['fg'] += offsetValue
-				offsetFg += 1
-			if 'bg' in additionalJson and int(additionalJson['bg']) > startingValue:
-				additionalJson['bg'] += offsetValue
-				offsetBg += 1
+	## Get offset value.
+	if len(sys.argv) < 4:
+		print(usageString)
+		sys.exit()
+	try:
+		offsetValue = int(sys.argv[3])
+	except ValueError:
+		print('Please specify an integer value for the offset value.')
+		sys.exit()
 
-## Save the output.
-outputFile = open(outputFilename, 'w')
-outputFile.write(json.dumps(loadedJson, indent=2, separators=(',', ': ')))
-outputFile.close()
+	## Get output filename.
+	if len(sys.argv) >= 4:
+		outputFilename = sys.argv[4]
 
-## All done.
-print('Offset ' + str(offsetFg) + ' fg and ' + str(offsetBg) + ' bg. File saved to ' + outputFilename + '.')
+	## Done with arg validation, process the json.
+	print('Offsetting all fg/bg values higher than ' + str(startingValue) + ' by ' + str(offsetValue) + ' in file ' + inputFilename + '.')
+	inputFile = open(inputFilename, 'r')
+	loadedJson = json.loads(inputFile.read(), object_pairs_hook = OrderedDict)
+	inputFile.close()
+
+	for jsonKey in loadedJson:
+		if 'tiles' in loadedJson[jsonKey][0]:
+			updateFgAndBgValuesInList(loadedJson[jsonKey][0]['tiles'], offsetValue, startingValue)
+
+	## Save the output.
+	outputFile = open(outputFilename, 'w')
+	outputFile.write(json.dumps(loadedJson, indent=2, separators=(',', ': ')))
+	outputFile.close()
+
+	## All done.
+	print('Offset ' + str(offsets['fg']) + ' fg and ' + str(offsets['bg']) + ' bg. File saved to ' + outputFilename + '.')
+
+if __name__ == '__main__':
+	main()
